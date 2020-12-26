5100543f0b6743266d69514e0706490a7a39500f
Hico <70553434+HicoMcD@users.noreply.github.com>
2020-12-26 21:27:31 +0700

    Update CricStatsCRUD.sol

--- contracts/CricStatsCRUD.sol
+++ contracts/CricStatsCRUD.sol
@@ -5,7 +5,10 @@ import "./SafeMath.sol";
 
 pragma solidity ^0.6.0;
 
-contract CricStatsCRUD is Ownable {
+/// @title CricStatsCRUD
+/// @author Hico
+/// @notice This contract has not been audited
+contract CRIC is Ownable {
     
     using SafeMath for uint;
 
@@ -20,9 +23,11 @@ contract CricStatsCRUD is Ownable {
     }
     
     CricPlayer[] public cricPlayers;
-    uint public nextCric = 1; 
+    uint public nextCric = 1;
     
-
+    uint public getBattingAvg;
+    uint public getBowlingAvg;
+    
     function createCric(string memory cricName, uint matchesPlayed, uint runsFor, uint runsAgainst, uint wicketsFor, uint wicketsAgainst) public onlyOwner() { 
         require(matchesPlayed >= wicketsAgainst, 'Cannot be dismissed more times than matches played');
         require(matchesPlayed > 0, 'Cannot have zero matches played');
@@ -60,18 +65,16 @@ contract CricStatsCRUD is Ownable {
         }
         revert('Cricket Player does not exist');
     }
-    
-    function getBattingAvg(uint256 runsFor, uint256 wicketsAgainst) public pure returns (uint256) {
-        require(wicketsAgainst > 0, 'Can only have an average if dismiised before');
-        uint256 battingAvg = runsFor / wicketsAgainst;
-        return battingAvg;
+
+    function setBattingAvg(uint256 _runsFor, uint256 _wicketsAgainst) public returns (uint256) {
+        require(_wicketsAgainst > 0, 'Can only have an average if dismiised before');
+        getBattingAvg = _runsFor.div(_wicketsAgainst);
+        return getBattingAvg;
     }
 
-    function getBowlingAvg(uint256 runsAgainst, uint256 wicketsFor) public pure returns (uint256) {
-        uint bowlingAvg = runsAgainst / wicketsFor;
-        return bowlingAvg;
+    function setBowlingAvg(uint256 _runsAgainst, uint256 _wicketsFor) public returns (uint256) {
+        getBowlingAvg = _runsAgainst.div(_wicketsFor);
+        return getBowlingAvg;
     }
 
 }
-
-
